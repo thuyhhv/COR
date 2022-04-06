2fd1bdd3e343e5249b78fb581481a641793a7e3c
thuy <thuyhhv.nta@gmail.com>
2022-04-06 22:29:00 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::group(['prefix'=>'user'],function(){
+	Route::get('/',[UserController::class, 'index'])->name('user');
+	Route::get('/create',[UserController::class, 'create'])->name('user.create');
+	Route::post('/create',[UserController::class, 'store'])->name('user.store');
+	Route::get('/edit/{id}',[UserController::class, 'edit'])->name('user.edit');
+	Route::post('/edit/{id}',[UserController::class, 'update'])->name('user.update');
+	Route::post('/delete/{id}',[UserController::class, 'delete'])->name('user.delete');
+});
