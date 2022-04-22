d19182b326353ccf4afd812657722216b1908408
thuy <thuyhhv.nta@gmail.com>
2022-04-22 15:27:44 +0700

    fix review

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserFormRequest extends FormRequest
+class StoreUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,9 +24,10 @@ class UserFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
-            'email'=> 'email|required'
-            'password' => 'required'
+            'user_name' => 'required|min:3|max:10',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password',
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,11 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::group(['prefix'=>'user'],function(){
-	Route::get('/',[UserController::class, 'index'])->name('user.index');
-	Route::get('/create',[UserController::class, 'create'])->name('user.create');
-	Route::post('/create',[UserController::class, 'store'])->name('user.store');
-	Route::get('/edit/{id}',[UserController::class, 'edit'])->name('user.edit');
-	Route::post('/edit/{id}',[UserController::class, 'update'])->name('user.update');
-	Route::post('/delete/{id}',[UserController::class, 'destroy'])->name('user.destroy');
+Route::group(['prefix'=>'user'], function() {
+	Route::get('/', [UserController::class, 'index'])->name('user.index');
+	Route::get('/create', [UserController::class, 'create'])->name('user.create');
+	Route::post('/create', [UserController::class, 'store'])->name('user.store');
+	Route::get('/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+	Route::post('/edit/{id}', [UserController::class, 'update'])->name('user.update');
+	Route::post('/delete/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 });
